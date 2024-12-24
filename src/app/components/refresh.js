eda3d7381fbaa9760ab6f5585a58db3df23bf0e6
Houjun Liu <houjun@jemoka.com>
2024-12-24 13:57:09 -0800

    automatic reload

--- src/app/components/refresh.js
+++ src/app/components/refresh.js
@@ -1,10 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import "../globals.css";
 import { useRouter }  from "next/navigation";
 
 export default function Refresh() {
     const router = useRouter();
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            router.refresh();
+        }, 1000);
+
+        return () => {clearInterval(interval);};
+    }, []);
 
     return (
         <a className="refresh-button" onClick={() => {
